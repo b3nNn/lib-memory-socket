ccdc0114681f811bf236bc3bc36c86d8af236a92
b3nNn <benoit1.martinet@gmail.com>
2025-02-24 05:25:39 +0100

    fix integration

--- memorysocket/memory_socket.cpp
+++ memorysocket/memory_socket.cpp
@@ -12,13 +12,46 @@ void producer()
     // Ringbuffer fully constructed in shared memory. The element strings are
     // also allocated from the same shared memory segment. This vector can be
     // safely accessed from other processes.
-    shm::ring_buffer *queue = segment.find_or_construct<shm::ring_buffer>("queue")();
+    shm::ring_buffer *queue = nullptr;
+
+    try {
+        queue = segment.find_or_construct<shm::ring_buffer>("queue")();
+    } catch (boost::interprocess::lock_exception& _) {
+        std::cerr << "mem lock exception" << std::endl;
+    } catch (std::exception& ex) {
+        std::cerr << "unknown exception: " << ex.what() << std::endl;
+    }
+
+    std::cout << "free memory: " << segment.get_free_memory() << std::endl;
+    std::cout << "memory size: " << segment.get_size() << std::endl;
+
+    if (queue == nullptr) {
+        return;
+    }
+
+    std::cout << "write available: " << queue->write_available() << std::endl;
 
     const char* messages[] = { "hello world", "the answer is 42", "where is your towel", 0 };
+    auto counter = 0;
 
-    for (const char** msg_it = messages; *msg_it; ++msg_it)
+    while (true)
     {
-        queue->push(shm::shared_string(*msg_it, char_alloc));
+        if ((counter % 1000000) == 0 && counter > 0)
+        {
+            std::cout << "produced " << counter / 1000000 << "M messages" << std::endl;
+        }
+
+        for (const char** msg_it = messages; *msg_it; ++msg_it)
+        {
+            if (!queue->push(shm::shared_string(*msg_it, char_alloc))) {
+                // std::cout << "free memory: " << segment.get_free_memory() << std::endl;
+                // std::cout << "memory size: " << segment.get_size() << std::endl;
+                // std::cout << "read available: " << queue->read_available() << std::endl;
+                // std::cout << "queue full" << std::endl;
+                break;
+            }
+            counter++;
+        }
     }
 }
 
@@ -29,12 +62,44 @@ void consumer()
     bip::managed_shared_memory segment(bip::open_or_create, "MySharedMemory", 65536);
     shm::char_alloc char_alloc(segment.get_segment_manager());
 
-    shm::ring_buffer *queue = segment.find_or_construct<shm::ring_buffer>("queue")();
+    shm::ring_buffer *queue = nullptr;
 
+    std::cout << "free memory: " << segment.get_free_memory() << std::endl;
+    std::cout << "memory size: " << segment.get_size() << std::endl;
+
+    try {
+        queue = segment.find_or_construct<shm::ring_buffer>("queue")();
+    } catch (boost::interprocess::lock_exception& _) {
+        std::cerr << "mem lock exception" << std::endl;
+    } catch (std::exception& ex) {
+        std::cerr << "unknown exception: " << ex.what() << std::endl;
+    }
+
+    if (queue == nullptr) {
+        try {
+            queue = segment.find_or_construct<shm::ring_buffer>("queue")();
+        } catch (std::exception& _) {
+            return;
+        }
+    }
+
+    std::cout << "read available: " << queue->read_available() << std::endl;
+
+    auto counter = 0;
     while (true)
     {
+        if ((counter % 1000000) == 0 && counter > 0)
+        {
+            std::cout << "consumed " << counter / 1000000 << "M messages" << std::endl;
+        }
+
         shm::shared_string v(char_alloc);
-        if (queue->pop(v))
-            std::cout << "Processed: '" << v << "'\n";
+        if (!queue->pop(v))
+        {
+            continue;
+            // std::cout << "Processed: '" << v << "'\n";
+        }
+
+        counter++;
     }
 }
--- memorysocket/memory_socket.h
+++ memorysocket/memory_socket.h
@@ -15,7 +15,7 @@ namespace shm
 
     typedef boost::lockfree::spsc_queue<
         shared_string,
-        boost::lockfree::capacity<200>
+        boost::lockfree::capacity<1000>
     > ring_buffer;
 }
 
